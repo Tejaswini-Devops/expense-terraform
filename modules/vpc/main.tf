0d59b7be80ed8756c99914a82962dca878ab1620
tejudevops <saitejaswini8121@gmail.com>
2025-04-19 00:34:34 +0530

    teeraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,3 +4,13 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-${var.project_name}-vpc"
   }
 }
+resource "aws_subnet" "main" {
+  count             = length(var.subnets_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnets_cidr[count.index]
+  availability_zone = var.az[count.index]
+
+  tags = {
+    Name = "${var.env}-${var.project_name}-subnet-${count.index}"
+  }
+}
