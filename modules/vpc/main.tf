9456dea99a312ad4b305ead45ed1c4cc34fa263e
tejudevops <saitejaswini8121@gmail.com>
2025-04-19 01:24:58 +0530

    teeraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_vpc_peering_connection" "main" {
 }
 resource "aws_route" "main" {
   route_table_id            = aws_vpc.main.main_route_table_id
-  destination_cidr_block    = data.aws_vpc.default.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
