a5f067cd57c516b0e6df8d071321ac0efe41fc23
tejudevops <saitejaswini8121@gmail.com>
2025-04-19 13:58:03 +0530

    teeraform

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_cidr" {}
 variable "env" {}
 variable "project_name" {}
-variable public_subnets_cidr {}
-variable "private_subnets_cidr" {}
+variable subnets_cidr {}
+# variable "private_subnets_cidr" {}
 variable "az" {}
