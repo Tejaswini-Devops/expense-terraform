a4475cc561859fb4d5dedd8dd334a098f6a05567
tejudevops <saitejaswini8121@gmail.com>
2025-04-20 14:48:17 +0530

    teeraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,18 +71,18 @@ resource "aws_nat_gateway" "main" {
 
 }
 
-resource "aws_subnet" "private" {
-  count             = length(var.private_subnets_cidr)
+resource "aws_subnet" "web" {
+  count             = length(var.web_subnets_cidr)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnets_cidr[count.index]
+  cidr_block        = var.web_subnets_cidr[count.index]
   availability_zone = var.az[count.index]
 
   tags = {
-    Name = "private_subnet-${count.index+1}"
+    Name = "web_subnet-${count.index+1}"
   }
 }
-resource "aws_route_table" "private" {
-  count             = length(var.public_subnets_cidr)
+resource "aws_route_table" "web" {
+  count             = length(var.web_subnets_cidr)
   vpc_id             = aws_vpc.main.id
 
   route {
@@ -95,14 +95,80 @@ resource "aws_route_table" "private" {
   }
 
   tags = {
-    Name = "private_rt-${count.index+1}"
+    Name = "web_rt-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "web" {
+  count          = length(var.web_subnets_cidr)
+  route_table_id = aws_route_table.web[count.index].id
+  subnet_id      = aws_subnet.web[count.index].id
+}
+resource "aws_subnet" "app" {
+  count             = length(var.app_subnets_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.app_subnets_cidr[count.index]
+  availability_zone = var.az[count.index]
+
+  tags = {
+    Name = "app_subnet-${count.index+1}"
+  }
+}
+resource "aws_route_table" "app" {
+  count             = length(var.app_subnets_cidr)
+  vpc_id             = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  tags = {
+    Name = "app_rt-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "app" {
+  count          = length(var.app_subnets_cidr)
+  route_table_id = aws_route_table.app[count.index].id
+  subnet_id      = aws_subnet.app[count.index].id
+}
+resource "aws_subnet" "db" {
+  count             = length(var.db_subnets_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets_cidr[count.index]
+  availability_zone = var.az[count.index]
+
+  tags = {
+    Name = "db_subnet-${count.index+1}"
+  }
+}
+resource "aws_route_table" "db" {
+  count             = length(var.db_subnets_cidr)
+  vpc_id             = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  tags = {
+    Name = "db_rt-${count.index+1}"
   }
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(var.private_subnets_cidr)
-  route_table_id = aws_route_table.private[count.index].id
-  subnet_id      = aws_subnet.private[count.index].id
+  count          = length(var.db_subnets_cidr)
+  route_table_id = aws_route_table.db[count.index].id
+  subnet_id      = aws_subnet.db[count.index].id
 }
 
 
@@ -116,40 +182,4 @@ resource "aws_route" "default" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-}
-
-data "aws_ami" "test" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = [973714476881]
-
-}
-resource "aws_security_group" "test" {
-  name        = "test"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = data.aws_ami.test.id
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.private[0].id
-  vpc_security_group_ids = [aws_security_group.test.id]
-  tags = {
-    Name = "test"
-  }
 }
