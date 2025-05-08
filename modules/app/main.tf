1495d58dd68bcf51d3eba88a9b06ac40cbf5c811
tejudevops <saitejaswini8121@gmail.com>
2025-05-08 17:02:05 +0530

    teeraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -60,8 +60,11 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
-  # health_check {
-  #   port     = 80
-  #   protocol = "HTTP"
-  # }
+  health_check {
+    path = "/health"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 2
+    timeout = 2
+  }
 }
