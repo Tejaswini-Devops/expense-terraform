1a1b8ad02bdd9e4f69d3eabf26fe83fed5f6c415
tejudevops <saitejaswini8121@gmail.com>
2025-05-08 22:51:13 +0530

    teeraform

--- modules/rds/data.tf
+++ modules/rds/data.tf
@@ -1,8 +1,8 @@
 data "aws_ssm_parameter" "username" {
-  name = "${var.env}-${var.project_name}.rds.username"
+  name = "${var.env}.${var.project_name}.rds.username"
 }
 
 data "aws_ssm_parameter" "password" {
-  name = "${var.env}-${var.project_name}.rds.password"
+  name = "${var.env}.${var.project_name}.rds.password"
   with_decryption = true
 }
