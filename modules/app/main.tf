11e1db47d06b49f268a1cf191b6fe07b0c526cb1
tejudevops <saitejaswini8121@gmail.com>
2025-05-21 17:27:11 +0530

    teeraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -36,10 +36,11 @@ resource "aws_launch_template" "main" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
 
-  user_data = base64encode(templatefile("${path.module}/userdata.sh",{
-    service_name = var.component
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    service_name = var.component,
     env          = var.env
   }))
+
 
   iam_instance_profile {
     name = aws_iam_instance_profile.main.name
--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Install Ansible and pip for Python 3.12
-dnf install -y ansible python3.12 python3.12-pip &>>/opt/userdata.log
+yum install -y ansible python3.11 python3.11-pip &>>/opt/userdata.log
+/usr/bin/python3.11 -m pip install boto3 botocore &>>/opt/userdata.log
 
-# Use pip for Python 3.12 explicitly
-/usr/bin/python3.12 -m pip install --upgrade pip &>>/opt/userdata.log
-/usr/bin/python3.12 -m pip install boto3 botocore &>>/opt/userdata.log
-
-# Optional: Ensure ansible uses Python 3.12
-export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3.12
-
-ansible-pull -i localhost, -U https://github.com/Tejaswini-Devops/expense-ansible expense.yml -e service_name=${service_name} -e env=${env} &>>/opt/userdata.log
+ansible-pull -i "localhost ansible_python_interpreter=/usr/bin/python3.11," \
+  -U https://github.com/Tejaswini-Devops/expense-ansible \
+  expense.yml \
+  -e service_name=${service_name} \
+  -e env=${env} &>>/opt/userdata.log
