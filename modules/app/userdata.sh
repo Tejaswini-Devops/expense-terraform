16cbe06949f6f03b0c3458de9f73d62ebb50b223
tejudevops <saitejaswini8121@gmail.com>
2025-05-21 17:16:03 +0530

    teeraform

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Install Ansible and pip for Python 3.12
-dnf install -y ansible python3.12-pip &>>/opt/userdata.log
+dnf install -y ansible python3.12 python3.12-pip &>>/opt/userdata.log
 
 # Use pip for Python 3.12 explicitly
-/usr/bin/pip3.12 install boto3 botocore &>>/opt/userdata.log
+/usr/bin/python3.12 -m pip install --upgrade pip &>>/opt/userdata.log
+/usr/bin/python3.12 -m pip install boto3 botocore &>>/opt/userdata.log
 
 # Optional: Ensure ansible uses Python 3.12
 export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3.12
