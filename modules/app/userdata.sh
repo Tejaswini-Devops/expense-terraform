ea0c075a5a3945a9d8079d1dd437e6fb84e46180
tejudevops <saitejaswini8121@gmail.com>
2025-05-21 16:55:59 +0530

    teeraform

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install ansible python3.11-pip -y &>>/opt/userdata.log
-pip3.11 install botocore boto3 &>>/opt/userdata.log
+yum install ansible python3.12-pip -y &>>/opt/userdata.log
+pip3.12 install botocore boto3 &>>/opt/userdata.log
 
 ansible-pull -i localhost, -U https://github.com/Tejaswini-Devops/expense-ansible expense.yml -e service_name=${service_name} -e env=${env} &>>/opt/userdata.log
