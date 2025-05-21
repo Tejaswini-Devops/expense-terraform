7562d0e6c4266ea8d7193b65495f93dc105d6108
tejudevops <saitejaswini8121@gmail.com>
2025-05-21 19:05:58 +0530

    teeraform

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-LOG_FILE="/opt/userdata.log"
-
-# Install Ansible and Python 3.12 with pip
-dnf install -y ansible python3.12 python3.12-pip &>>"$LOG_FILE"
-
-# Install boto3 and botocore for Python 3.12 (NOT 3.11)
-# Use absolute path to ensure you're installing to the correct interpreter
-/usr/bin/python3.12 -m pip install boto3 botocore &>>"$LOG_FILE"
-
-# Confirm the libraries are installed
-/usr/bin/python3.12 -c "import boto3, botocore; print('✅ boto3 and botocore installed')" &>>"$LOG_FILE"
-
-# Run ansible-pull with Python 3.12 explicitly set
-ansible-pull -i "localhost ansible_python_interpreter=/usr/bin/python3.12" \
-  -U https://github.com/Tejaswini-Devops/expense-ansible \
-  expense.yml \
-  -e service_name=${service_name} \
-  -e env=${env} &>>"$LOG_FILE"
+yum install ansible python3.11-pip -y &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/raghudevopsb77/expense-ansible expense.yml -e service_name=${service_name} -e env=${env} &>>/opt/userdata.log
